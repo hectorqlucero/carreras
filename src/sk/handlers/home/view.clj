6f67950261def0da4f9b333d57adf287dd749c7e
Hector Lucero <hectorqlucero@gmail.com>
2021-08-23 14:51:01 -0700

    Removed unreferenced library

--- src/sk/handlers/home/view.clj
+++ src/sk/handlers/home/view.clj
@@ -1,7 +1,6 @@
 (ns sk.handlers.home.view
   (:require
     [hiccup.page :refer [include-js]]
-    [sk.models.crud :refer [config]]
     [sk.models.util :refer [build-form
                             build-field
                             build-radio-buttons
